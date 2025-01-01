070d885490ff30c02ac02fe5d19e00e1b7ce5c70
moryoav <moryoav@gmail.com>
2025-01-01 09:11:09 +0200

    Update whatsapp.py
    Return value instead of ok

--- whatsapp_addon/custom_component/whatsapp.py
+++ whatsapp_addon/custom_component/whatsapp.py
@@ -5,7 +5,9 @@ HOST = 'http://{{HOSTNAME}}:3000/'
 
 class Whatsapp:
     def send_message(self, data):
-        return requests.post(url_normalize(f'{HOST}/sendMessage'), json=data).content == 'OK'
+        r = requests.post(url_normalize(f'{HOST}/sendMessage'), json=data)
+        r.raise_for_status()
+        return r.json()
 
     def set_status(self, data):
         return requests.post(url_normalize(f'{HOST}/setStatus'), json=data).content == 'OK'
